b0c35020d324c7bfb4a996a7f93ffbc6e17b7c28
Fasil Kebede <46403603+FasilGibdaw@users.noreply.github.com>
2025-03-11 10:20:21 +0100

    numpy.distutils.core deprecated error fix

--- setup.py
+++ setup.py
@@ -1,46 +1,75 @@
-#!/usr/bin/env python
-req = ['nose','numpy','pathlib2',
-       'timeutil']
-# %%
-from setuptools import find_packages
-from numpy.distutils.core import Extension, setup
+from setuptools import setup, find_packages
+import numpy
 from glob import glob
-from os.path import join
+from os.path import join, abspath
+import subprocess
+import os
+import shutil
+
 
 name = 'pyhwm2014'
 
-ext = Extension( extra_compile_args=['-w'],
-            extra_f90_compile_args=['-w'],
-            f2py_options=[ '--quiet' ],
-            name='hwm14',
-            sources=['source/hwm14.f90']
-             )
+# Define the Fortran source file
+fortran_source = 'source/hwm14.f90'
+package_folder = os.path.join(name)
 
-hwmData1 = glob(join('data', '*.dat'))
-hwmData2 = glob(join('data', '*.bin'))
-hwmDataFiles = [(join(name, 'data'), hwmData1),
-                (join(name, 'data'), hwmData2)]
+# Manually invoke f2py for the Fortran compilation
+def build_extension():
+    # Compile the Fortran file using f2py
+    subprocess.check_call([
+        'f2py', 
+        '-c',  # Compile only
+        '-m', 'hwm14',  # Module name (this creates PyInit_hwm14)
+        fortran_source,  # Fortran source file
+        '--f90flags=-w',  # Suppress warnings (optional)
+        '--quiet',  # Quiet mode
+        '-I{}'.format(numpy.get_include())  # Include NumPy headers
+    ])
+    compiled_files = glob('*.so')
+    if compiled_files:
+      for compiled_file in compiled_files:
+        compiled_file_path = abspath(compiled_file)
+        # Copy the compiled .so file into the package directory
+        shutil.copy(compiled_file_path, package_folder)
+        print(f"Successfully copied {compiled_file} to {package_folder}")
+    else:
+        raise FileNotFoundError("No compiled .so file found!")
 
-setup( author=['Ronald Ilma'],
-        data_files=hwmDataFiles,
-        description='HWM14 neutral winds model',
-        ext_modules=[ ext ],
-        ext_package=name,
-        name=name,
-        packages=find_packages(),
-        url='https://github.com/rilma/pyHWM14',
-        version='1.1.0',
-        install_requires=req,
-        extras_requires={'plot':['matplotlib','seaborn']},
-        dependency_links=[
-      'https://github.com/rilma/TimeUtilities/zipball/master#egg=timeutil-999.0'],
-        classifiers=[
-          'Intended Audience :: Science/Research',
-          'Development Status :: 5 - Production/Stable',
-          'License :: OSI Approved :: MIT License',
-          'Topic :: Scientific/Engineering :: Atmospheric Science',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          ],
-         python_requires='>=3.10',
+
+# Call the build_extension function to compile the Fortran code
+build_extension()
+
+# Collect data files
+hwmData1 = glob(join("data", "*.dat"))
+hwmData2 = glob(join("data", "*.bin"))
+hwmDataFiles = [(join(name, "data"), hwmData1),
+                (join(name, "data"), hwmData2)]
+
+# Define dependencies
+req = ["nose", "numpy", "pathlib2", "timeutil"]
+
+setup(
+    author="Ronald Ilma",
+    data_files=hwmDataFiles,
+    description="HWM14 neutral winds model",
+    ext_modules=[],  # No need to define the extension here, f2py handles it
+    ext_package=name,
+    name=name,
+    packages=find_packages(),
+    url="https://github.com/rilma/pyHWM14",
+    version="1.1.0",
+    install_requires=req,
+    extras_require={"plot": ["matplotlib", "seaborn"]},
+    dependency_links=[
+        "https://github.com/rilma/TimeUtilities/zipball/master#egg=timeutil-999.0"
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.10",
 )
